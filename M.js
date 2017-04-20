6ed4fd606255b9e4fa5f15c55e390a2aeb0815d2
suhan1996 <hs2657@nyu.edu>
2017-04-19 20:57:31 -0400

    hi

--- M.js
+++ M.js
@@ -1,159 +1,55 @@
+M = (function() {
+   var my = {};
+   var stack = [];
 
-//////////////////////////////////////////////////////////////////////////////
-// M is an object containing methods that let you manipulate 4x4 matrices.
-//////////////////////////////////////////////////////////////////////////////
+   my.copy        = function(m, src)   { for (var i = 0 ; i < src.length ; i++) m[i] = src[i]; return m; }
+   my.identity    = function(m)        { for (var i = 0 ; i<16 ; i++) m[i] = i % 5 ? 0 : 1; return m; }
+   my.restore     = function(m)        { my.copy(m, stack.pop()); }
+   my.rotateX     = function(m, a)     { return my.matrixMultiply(m, my.rotateXMatrix  (a)     , m); }
+   my.rotateY     = function(m, a)     { return my.matrixMultiply(m, my.rotateYMatrix  (a)     , m); }
+   my.rotateZ     = function(m, a)     { return my.matrixMultiply(m, my.rotateZMatrix  (a)     , m); }
+   my.save        = function(m)        { stack.push(my.copy([], m)); return m; }
+   my.scale       = function(m, x,y,z) { return my.matrixMultiply(m, my.scaleMatrix    (x,y,z) , m); }
+   my.translate   = function(m, v)     { my.matrixMultiply(m, my.translationMatrix(v), m); }
 
-var M = {};
-M._stack = [];
+   my.identityMatrix = function()      { return [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1]; }
+   my.inverseMatrix  = function(src)   {
+      function s(col, row) { return src[col & 3 | (row & 3) << 2]; }
+      function cofactor(c0, r0) {
+         var c1 = c0+1, c2 = c0+2, c3 = c0+3, r1 = r0+1, r2 = r0+2, r3 = r0+3;
+         return (c0 + r0 & 1 ? -1 : 1) * ( (s(c1, r1) * (s(c2, r2) * s(c3, r3) - s(c3, r2) * s(c2, r3)))
+                                         - (s(c2, r1) * (s(c1, r2) * s(c3, r3) - s(c3, r2) * s(c1, r3)))
+                                         + (s(c3, r1) * (s(c1, r2) * s(c2, r3) - s(c2, r2) * s(c1, r3))) );
+      }
+      var n, dst = [], det = 0;
+      for (n = 0 ; n < 16 ; n++) dst.push(cofactor(n >> 2, n & 3));
+      for (n = 0 ; n <  4 ; n++) det += src[n] * dst[n << 2];
+      for (n = 0 ; n < 16 ; n++) dst[n] /= det;
+      return dst; }
+   my.rotateXMatrix  = function(a)     { return [1,0,0,0, 0,Math.cos(a),Math.sin(a),0, 0,-Math.sin(a),Math.cos(a),0, 0,0,0,1]; }
+   my.rotateYMatrix  = function(a)     { return [Math.cos(a),0,-Math.sin(a),0, 0,1,0,0, Math.sin(a),0,Math.cos(a),0, 0,0,0,1]; }
+   my.rotateZMatrix  = function(a)     { return [Math.cos(a),Math.sin(a),0,0, -Math.sin(a),Math.cos(a),0,0, 0,0,1,0, 0,0,0,1]; }
+   my.scaleMatrix    = function(x,y,z) { if (x instanceof Array) { x = x[0]; y = x[1]; y = x[2]; }
+                                        else if (y === undefined) z = y = x;
+                                        return [x,0,0,0, 0,y,0,0, 0,0,z,0, 0,0,0,1]; }
+   my.translationMatrix = function(v) { return [ 1,0,0,0, 0,1,0,0, 0,0,1,0, v[0],v[1],v[2],1 ]; }
+   my.matrixMultiply = function(a, b, dst) {
+      var n, tmp = [];
+      for (n = 0 ; n < 16 ; n++)
+         tmp.push(a[n&3     ] * b[    n&12] +
+                  a[n&3 |  4] * b[1 | n&12] +
+   	       a[n&3 |  8] * b[2 | n&12] +
+   	       a[n&3 | 12] * b[3 | n&12] );
+      for (n = 0 ; n < 16 ; n++)
+         dst[n] = tmp[n];
+   }
+   my.transform = function(m, v)  {
+       var x = v[0], y = v[1], z = v[2], w = v[3] === undefined ? 1 : v[3];
+       return [ x * m[0] + y * m[4] + z * m[ 8] + w * m[12],
+                x * m[1] + y * m[5] + z * m[ 9] + w * m[13],
+                x * m[2] + y * m[6] + z * m[10] + w * m[14],
+                x * m[3] + y * m[7] + z * m[11] + w * m[15] ]; }
 
-//////////////////////////////////////////////////////////////////////////////
-// Your task is to implement the following methods of object M:
-//////////////////////////////////////////////////////////////////////////////
-
-M.identity  = function(m)          {
-    let a = [1,0,0,0,
-         0,1,0,0,
-         0,0,1,0,
-         0,0,0,1];
-    for (let i = 0; i<m.length;i++){
-        m[i] = a[i];
-    }
-    //console.log("mmmmmm",m);
-
-    //return m;
-} // Set m values to identity matrix.
-
-M.restore   = function(m)          {
-    let _m = M._stack.pop();
-    for(let i = 0; i<m.length;i++){
-        m[i] = _m[i];
-    }
-    //return m;
-} // Pop from a stack to set the 16 values of m.
-
-M.rotateX   = function(m, radians) {
-    //console.log("11111",m,radians);
-    M.matrixMultiply(m, [1,0,0,0, 0,Math.cos(radians),-Math.sin(radians),0, 0,Math.sin(radians),Math.cos(radians),0, 0,0,0,1], m);
-    //console.log("2222",m);
-
-      /*
-     let a = [0,0,0,0, 0,Math.cos(radians),-Math.sin(radians),0, 0,Math.sin(radians),Math.cos(radians),0, 0,0,0,0];
-       for (let i = 0; i<m.length;i++){
-           m[i] = a[i];
-       }
-       */
-
-} // Modify m, rotating about the X axis.
-
-M.rotateY   = function(m, radians) {
-    /*
-    let a = [Math.cos(radians),0,Math.sin(radians),0, 0,0,0,0, -Math.sin(radians),0,Math.cos(radians),0, 0,0,0,0];
-
-     for (let i = 0; i<m.length;i++){
-         m[i] = a[i];
-     }
-      */
-    M.matrixMultiply(m, [Math.cos(radians),0,-Math.sin(radians),0, 0,1,0,0, Math.sin(radians),0,Math.cos(radians),0, 0,0,0,1], m);
-    //console.log("3333",m);
-
-} // Modify m, rotating about the Y axis.
-
-M.rotateZ   = function(m, radians) {
-   /*
-     let a  = [Math.cos(radians),-Math.sin(radians),0,0, Math.sin(radians),Math.cos(radians),0,0, 0,0,0,0, 0,0,0,0];
-    let a =    M.matrixMultiply(m, [Math.cos(radians),-Math.sin(radians),0,0, Math.sin(radians),Math.cos(radians),0,0, 0,0,0,0, 0,0,0,0], m);
-
-     for (let i = 0; i<m.length;i++){
-         m[i] = a[i];
-     }
-   */
-    M.matrixMultiply(m, [Math.cos(radians),Math.sin(radians),0,0, -Math.sin(radians),Math.cos(radians),0,0, 0,0,1,0, 0,0,0,1], m);
-    //console.log(m);
-
-} // Modify m, rotating about the Z axis.
-
-M.save     = function(m)          {
-    let _m = [];
-    m.forEach(x=>_m.push(x));
-    M._stack.push(_m);
-    //console.log(M._stack)
-} // Push the 16 values of m onto a stack.
-M.scale = function(m, v)      {
-
-    var x,y,z,a;
-
-       if (v instanceof Array) {
-          x = v[0];
-          y = v[1];
-          z = v[2];
-           M.matrixMultiply(m, [v[0],0,0,0, 0,v[1],0,0, 0,0,v[2],0, 0,0,0,0] , m);
-           a = [v[0],0,0,0, 0,v[1],0,0, 0,0,v[2],0, 0,0,0,0] ;
-
-       }
-       else {
-           x = y = z = v;
-           M.matrixMultiply(m, [v,0,0,0, 0,v,0,0, 0,0,v,0, 0,0,0,0] , m);
-           a = [v,0,0,0, 0,v,0,0, 0,0,v,0, 0,0,0,0] ;
-
-       }
-
-
-console.log("first",m)
-
-     for (let i = 0; i<m.length;i++){
-         m[i] = a[i];
-     }
-
-
-} // Modify m, scaling by v[0],v[1],v[2].
-//M.transform = function(m, v)       {
-//
-//    return m;
-//} // Return vec v transformed by matrix m.
-//M.translate = function(m, v)       {           } // Modify m, translating by v[0],v[1],v[2].
-
-//////////////////////////////////////////////////////////////////////////////
-// I have given you a head start by implementing some of the methods for you.
-//
-// Notice how I use M.matrixMultiply() to help implement other methods.
-//////////////////////////////////////////////////////////////////////////////
-
-M.translate = function(m, v) {
-   M.matrixMultiply(m, M.translationMatrix(v), m);
-}
-
-M.translationMatrix = function(v) {
-   return [ 1,0,0,0, 0,1,0,0, 0,0,1,0, v[0],v[1],v[2],1 ];
-}
-
-M.matrixMultiply = function(a, b, dst) {
-   var n, tmp = []; 
-
-   // PUT THE RESULT OF a x b INTO TEMPORARY MATRIX tmp.
-
-   for (n = 0 ; n < 16 ; n++)
-      tmp.push( a[n&3     ] * b[    n&12] +
-                a[n&3 |  4] * b[1 | n&12] +
-                a[n&3 |  8] * b[2 | n&12] +
-                a[n&3 | 12] * b[3 | n&12] );
-
-   // COPY tmp VALUES INTO DESTINATION MATRIX dst.
-
-   for (n = 0 ; n < 16 ; n++)
-      dst[n] = tmp[n];
-}
-
-M.transform = function(m, v)  {
-
-    // IF v[3] IS UNDEFINED, SET IT TO 1 (THAT IS, ASSUME v IS A POINT).
-
-    var x = v[0], y = v[1], z = v[2], w = v[3] === undefined ? 1 : v[3];
-
-    // RETURN RESULT OF TRANSFORMING v BY MATRIX m.
-
-    return [ x * m[0] + y * m[4] + z * m[ 8] + w * m[12],
-             x * m[1] + y * m[5] + z * m[ 9] + w * m[13],
-             x * m[2] + y * m[6] + z * m[10] + w * m[14],
-             x * m[3] + y * m[7] + z * m[11] + w * m[15] ];
-}
+   return my;
+})();
 
